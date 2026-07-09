5dd15bacebc2bfd19aae1f7af0609bc6199ebfa2
Vinhnguyen0801 <137134831+Vinhnguyen0801@users.noreply.github.com>
2026-07-09 09:58:52 +0700

    Version 0.3

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 const chooseBtn = document.getElementById("chooseBtn");
-
+const resetBtn = document.getElementById("resetBtn");
 const fileInput = document.getElementById("fileInput");
-
 const previewImage = document.getElementById("previewImage");
-
 const placeholder = document.getElementById("placeholder");
 
+let cropper = null;
+
+// Chọn ảnh
 chooseBtn.addEventListener("click", () => {
-
     fileInput.click();
-
 });
 
-fileInput.addEventListener("change", function(){
+// Khi chọn ảnh
+fileInput.addEventListener("change", function () {
 
     const file = this.files[0];
 
-    if(!file) return;
+    if (!file) return;
 
     const reader = new FileReader();
 
-    reader.onload = function(e){
+    reader.onload = function (e) {
 
         previewImage.src = e.target.result;
 
@@ -28,8 +28,63 @@ fileInput.addEventListener("change", function(){
 
         placeholder.style.display = "none";
 
-    }
+        previewImage.onload = function () {
+
+            if (cropper) {
+                cropper.destroy();
+            }
+
+            cropper = new Cropper(previewImage, {
+
+                aspectRatio: 1,
+
+                viewMode: 1,
+
+                dragMode: "move",
+
+                autoCropArea: 1,
+
+                background: false,
+
+                guides: false,
+
+                center: false,
+
+                highlight: false,
+
+                cropBoxMovable: false,
+
+                cropBoxResizable: false,
+
+                movable: true,
+
+                zoomable: true,
+
+                rotatable: false,
+
+                scalable: false,
+
+                wheelZoomRatio: 0.1,
+
+                responsive: true
+
+            });
+
+        };
+
+    };
 
     reader.readAsDataURL(file);
+
+});
+
+// Đặt lại
+resetBtn.addEventListener("click", () => {
+
+    if (cropper) {
+
+        cropper.reset();
+
+    }
 
 });
