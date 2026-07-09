7bdc754fb86202b1a4b709ff6d891cf30c1ca55a
Vinhnguyen0801 <137134831+Vinhnguyen0801@users.noreply.github.com>
2026-07-09 09:51:18 +0700

    Upload image feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log("Avatar Website Ready!");
+const chooseBtn = document.getElementById("chooseBtn");
+
+const fileInput = document.getElementById("fileInput");
+
+const previewImage = document.getElementById("previewImage");
+
+const placeholder = document.getElementById("placeholder");
+
+chooseBtn.addEventListener("click", () => {
+
+    fileInput.click();
+
+});
+
+fileInput.addEventListener("change", function(){
+
+    const file = this.files[0];
+
+    if(!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function(e){
+
+        previewImage.src = e.target.result;
+
+        previewImage.style.display = "block";
+
+        placeholder.style.display = "none";
+
+    }
+
+    reader.readAsDataURL(file);
+
+});
